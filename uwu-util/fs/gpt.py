dad428bf338af71deb9aabaf008b17a63c537339
Aki Van Ness <aki@lethalbit.net>
2021-07-12 02:49:33 -0400

    uwu-util: gpt: Fixed up the GPT structures some to actually work, got rid of the janky mk_ methods, planning to re-write all of that

--- uwu-util/fs/gpt.py
+++ uwu-util/fs/gpt.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: BSD-3-Clause
 
+from zlib import crc32
+from uuid import uuid4
+
 if __name__ == '__main__':
 	from guids import GPT_PART_TYPES
 else:
@@ -9,125 +12,113 @@ else:
 
 from construct import *
 
-guid = Padded(16, Struct(
-	'data0' / Int32ul,
-	'data1' / Int16ul,
-	'data2' / Int16ul,
-	'data3' / Int16ul,
-	'data4' / Bytes(6)
+ASSUMED_BLOCK_SIZE = 512
+
+"""
+
+"""
+guid = 'GUID' / Padded(16, Struct(
+	'data0' / Hex(Int32ul),                        # ???
+	'data1' / Hex(Int16ul),                        # ???
+	'data2' / Hex(Int16ul),                        # ???
+	'data3' / Hex(Int16ub),                        # ???
+	'data4' / Bytes(6)                             # ???
 ))
 
-partition_record = Struct(
-	'boot_indicator' / Bytes(1), 		#	Boot indicator: 0x80 for bootable
-	'starting_chs'   / Bytes(3),		#	Start of partition in CHS format
-	'os_type'        / Bytes(1),		#	OS Type (0xEF or 0xEE)
-	'ending_chs'     / Bytes(3),		# 	End of partition in CHS format
-	'starting_lba'   / Int32ul,			# 	Starting LBA of the partition
-	'size_in_lba'    / Int32ul,			#	Size of partition in LBA units
+"""
+
+"""
+partition_record = 'Partition Record' / Struct(
+	'boot_indicator' / Const(b'\x00'),             # Boot indicator: 0x00
+	'starting_chs'   / Hex(Bytes(3)),	           # Start of partition in CHS format
+	'os_type'        / Hex(Bytes(1)),	           # Indicated Partition type
+	'ending_chs'     / Hex(Bytes(3)),	           # End of partition in CHS format
+	'starting_lba'   / Hex(Int32ul),	           # Starting LBA of the partition
+	'size_in_lba'    / Hex(Int32ul),	           # Size of partition in LBA units
 )
 
-protective_mbr = Struct(
-	'boot_code'      / Bytes(440),						# boot code for non-UEFI system
-	'disk_signature' / Const(b'\x00\x00\x00\x00'),		# Unused, must be 0
-	'reserved'       / Const(b'\x00\x00'),				# Unused, must be 0
-	'part_records'   / Array(4, partition_record),		# Partition Records (*4)
-	'signature'      / Const(b'\x55\xAA'),				# Boot signature
-)
+"""
 
-gpt_header = Padded(int(512), Struct(
-	'signature'     / Const(b'\x54\x52\x41\x50\x20\x49\x46\x45'),
-	'revision'      / Const(b'\x00\x01\x00\x00'),
-	'header_size'   / Int32ul,
-	'crc32'         / Int32ul,
-	Padding(4),
-	'my_lba'        / Int64ul,
-	'alt_lba'       / Int64ul,
-	'fusable_lba'   / Int64ul,
-	'luseable_lba'  / Int64ul,
-	'disk_guid'     / guid,
-	'part_count'    / Int32ul,
-	'part_ent_size' / Int32ul,
-	'part_ents_crc' / Int32ul,
+"""
+protective_mbr = 'Protective MBR' / Padded(ASSUMED_BLOCK_SIZE, Struct(
+	'boot_code'      / Bytes(440),				   # boot code for non-UEFI system
+	'disk_signature' / Const(b'\x00\x00\x00\x00'), # Unused, must be 0
+	'reserved'       / Const(b'\x00\x00'),		   # Unused, must be 0
+	'part_records'   / Array(4, partition_record), # Partition Records (*4)
+	'signature'      / Const(b'\x55\xAA'),		   # Boot signature
 ))
 
+"""
 
+"""
+gpt_header = 'GPT Header' / Padded(ASSUMED_BLOCK_SIZE, Struct(
+	'signature'     / Const(b'EFI PART'),          # GPT Magic Signature
+	'revision'      / Const(b'\x00\x00\x01\x00'),  # GPT Version
+	'header_size'   / Hex(Int32ul),				   # 92 < GPT Header Size <=LBA Size
+	'crc32'         / Hex(Int32ul),				   # CRC of header w/ this set to 0
+	'reserved'      / Padding(4),                  # 4-byte reserved field
+	'my_lba'        / Hex(Int64ul),				   # LBA of this header
+	'alt_lba'       / Hex(Int64ul),                # LBA of backup header
+	'fusable_lba'   / Hex(Int64ul),				   # First Usable LBA
+	'luseable_lba'  / Hex(Int64ul),				   # Last Usable LBA
+	'disk_guid'     / guid,						   # Disk GUID
+	'part_end_lba'  / Hex(Int64ul),				   # Starting LBA of Partition table
+	'part_count'    / Hex(Int32ul),				   # Number of Partition Entries
+	'part_ent_size' / Hex(Int32ul),				   # Size of Partition Entries
+	'part_ents_crc' / Hex(Int32ul),				   # Partition Array CRC32
+))
 
-gpt_entry = Struct(
-	'type_guid' / guid,
-	'part_guid' / guid,
-	'start_lba' / Int64ul,
-	'end_lba'   / Int64ul,
-	'name'      / PaddedString(72, 'utf8'),
+"""
+
+"""
+gpt_entry = 'GPT Entry' / Struct(
+	'type_guid'  / guid,						   # Partition Type GUID
+	'part_guid'  / guid,						   # Partition GUID
+	'start_lba'  / Hex(Int64ul),				   # Starting LBA for partition
+	'end_lba'    / Hex(Int64ul),				   # Ending LBA for partition
+	'attributes' / BitStruct(					   # Attributes bitfield
+		'required'    / Flag,					   #  * This partition is required for platform to function
+		'no_block_io' / Flag,                      #  * Firmware can not EFI_BLOCK_IO_PROTOCOL
+		'legacy_boot' / Flag,                      #  * Possibly allow legacy boot?
+		'reserved'    / Padding(45),               #  * Reserved for future flags
+		'guid_res'    / BitsInteger(16)),          #  * Partition Type GUID specific use
+	'name'       / PaddedString(72, 'utf16'),      # Partition Name
 )
 
-gpt_disk_image = Struct(
-	'mbr' / protective_mbr, # Guard MBR for GPT image
+"""
+
+"""
+gpt_disk_image = 'GPT Disk Image' / Struct(
+	'protective_mbr'   / protective_mbr,           # Guard MBR for GPT image        | LBA0
+	'primary_pheader'  / gpt_header,               # Primary partition table header | LBA1
+	'primary_pentries' / Array(
+		this.primary_pheader.part_count,
+		Padded(
+			this.primary_pheader.part_ent_size,
+			gpt_entry
+		)
+	),                                             # Partition Entries
+	'disk_data' / LazyArray(
+		(this.primary_pheader.luseable_lba + 1) - this.primary_pheader.fusable_lba,
+		Bytes(ASSUMED_BLOCK_SIZE)
+	),                                             # Disk Data
+
+	'backup_pentries' / Array(
+		this.primary_pheader.part_count,
+		Padded(
+			this.primary_pheader.part_ent_size,
+			gpt_entry
+		)
+	),                                             # Backup Partition Entries
+	'backup_pheader' / gpt_header                  # Backup GPT Header | LBAn
 )
 
-def mk_protective_mbr(*, lba_count, lba_size = 4096, last_block = b'\xFF\xFF\xFF', nest = False, pad = True):
-	lba_padded_mbr = Padded(lba_size, protective_mbr)
-
-	mbr_dict = {
-		'boot_code': b'\x00' * 440,
-		'part_records': [
-			mk_protective_rec(
-				lba_count  = lba_count,
-				lba_size   = lba_size,
-				last_block = last_block,
-				nest       = True
-			),
-			mk_protective_rec(
-				lba_count  = lba_count,
-				lba_size   = lba_size,
-				last_block = last_block,
-				nest       = True
-			),
-			mk_protective_rec(
-				lba_count  = lba_count,
-				lba_size   = lba_size,
-				last_block = last_block,
-				nest       = True
-			),
-			mk_protective_rec(
-				lba_count  = lba_count,
-				lba_size   = lba_size,
-				last_block = last_block,
-				nest       = True
-			),
-		]
-	}
-
-	if not nest:
-		if pad:
-			return lba_padded_mbr.build(mbr_dict)
-		else:
-			return protective_mbr.build(mbr_dict)
-	else:
-		return mbr_dict
-
-def mk_protective_rec(*, lba_count, lba_size = 4096, last_block = b'\xFF\xFF\xFF', nest = False):
-	rec_dict = {
-		'boot_indicator' : b'\x00',
-		'starting_chs'   : b'\x00\x02\x00',
-		'os_type'        : b'\xEE',
-		'ending_chs'     : last_block,
-		'starting_lba'   : 1,
-		'size_in_lba'    : lba_count * lba_size
-	}
-
-	if not nest:
-		return partition_record.build(rec_dict)
-	else:
-		return rec_dict
-
-
-
-# def make_empty_gpt_image(*, lba_count, lba_size = 4096)
 
 
 if __name__ == '__main__':
 	def dump_img(file_name):
-		return 0
+		with open(file_name, 'rb') as f:
+			print(gpt_disk_image.parse(f.read()))
 
 	import sys
 	from os import path
